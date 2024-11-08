e4f5a5659c998c63730c080acd6762b8aef5c047
Prana-vvb <pvb1532005@gmail.com>
2024-11-08 20:16:34 +0530

    refactor upload

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
+use futures::{StreamExt, TryStreamExt};
 use std::convert::Infallible;
 use std::fs::{self, File};
 use std::io::{self, Write};
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use uuid::Uuid;
-use warp::Filter;
+use warp::{Filter, Rejection, Reply};
 
 const SITE: &str = "http://localhost:8080";
-const MAX_SIZE: usize = 1 * 1024 * 1024;
+const MAX_SIZE: u64 = 1 * 1024 * 1024;
 
 #[tokio::main]
 async fn main() {
-    fs::create_dir_all("data").expect("Unable to create data directory");
-
-    let upload = warp::post()
-        .and(warp::path::end())
-        .and(warp::multipart::form())
+    let upload = warp::path("data")
+        .and(warp::post())
+        .and(warp::multipart::form().max_length(MAX_SIZE))
         .and_then(handle_upload);
 
     let disp = warp::get()
-        .and(warp::path!("data" / String));
-        //.and_then(handle_disp);
+        .and(warp::path!("data" / String))
+        .and_then(handle_disp);
 
     //Why block bots tho
     let robots_txt = warp::path("robots.txt").map(|| {
@@ -34,45 +33,12 @@ async fn main() {
 }
 
 async fn handle_upload(data: warp::multipart::FormData) -> Result<impl warp::Reply, Infallible> {
-    //if data.len() > MAX_SIZE {
-    //    return Ok(warp::reply::with_status(
-    //        "File size cannot be > 1 MB",
-    //        warp::http::StatusCode::BAD_REQUEST,
-    //    ));
-    //}
+    let mut parts = data.into_stream();
 
-    let parts = data.files().next();
-    let file = match parts {
-        Some(part) => part,
-        None => {
-            return Ok(warp::reply::with_status(
-                "Failed to get file",
-                warp::http::StatusCode::BAD_REQUEST,
-            ))
-        }
-    };
-
-    if file.len() == 0 {
-        return Ok(warp::reply::with_status(
-            "File is empty",
-            warp::http::StatusCode::BAD_REQUEST,
-        ));
+    while let Some(Ok(p)) = parts.next().await {
+        let value = p.stream().try_fold(Vec::new(), |mut vec, data| {
+            vec.put(data);
+            async move { Ok(vec) }
+        }).await
     }
-
-    let id = Uuid::new_v4().to_string();
-    let new_file_path = format!("data/{}", id);
-    let mut new_file = match File::create(&new_file_path) {
-        Ok(file) => file,
-        Err(_) => {
-            return Ok(warp::reply::with_status(
-                "Couldn't save file",
-                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
-            ));
-        }
-    };
-
-    Ok(warp::reply::with_status(
-        &format!("File pasted at {}/data/{}", SITE, id),
-        warp::http::StatusCode::OK,
-    ))
 }
