3412f1dde6da3ad3e627f7f03d984013112222bd
Prana-vvb <pvb1532005@gmail.com>
2024-11-08 08:59:29 +0530

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::convert::Infallible;
-use std::fs;
+use std::fs::{self, File};
 use std::io::{self, Write};
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
-use tokio::fs;
 use uuid::Uuid;
-use warp::{wrap_fn, Filter};
+use warp::Filter;
 
 const SITE: &str = "http://localhost:8080";
-const MAX_SIZE: usize = 1024 * 1024;
+const MAX_SIZE: usize = 1 * 1024 * 1024;
 
 #[tokio::main]
 async fn main() {
@@ -20,24 +19,27 @@ async fn main() {
         .and_then(handle_upload);
 
     let disp = warp::get()
-        .and(warp::path!("data" / String))
-        .and_then(handle_disp);
+        .and(warp::path!("data" / String));
+        //.and_then(handle_disp);
 
-    let routes = upload.or(disp);
+    //Why block bots tho
+    let robots_txt = warp::path("robots.txt").map(|| {
+        warp::reply::with_header("User-agent: *\nDisallow: /", "Content-Type", "text/plain")
+    });
 
-    //What da robot do
+    let routes = upload.or(disp).or(robots_txt);
 
     let port = 8080;
     warp::serve(routes).run(([127, 0, 0, 1], port)).await;
 }
 
-async fn handle_upload(data: warp::multipart::Form) -> Result<impl warp::Reply, Infallible> {
-    if data.len() > MAX_SIZE {
-        return Ok(warp::reply::with_status(
-            "File size cannot be > 1 MB",
-            warp::http::StatusCode::BAD_REQUEST,
-        ));
-    }
+async fn handle_upload(data: warp::multipart::FormData) -> Result<impl warp::Reply, Infallible> {
+    //if data.len() > MAX_SIZE {
+    //    return Ok(warp::reply::with_status(
+    //        "File size cannot be > 1 MB",
+    //        warp::http::StatusCode::BAD_REQUEST,
+    //    ));
+    //}
 
     let parts = data.files().next();
     let file = match parts {
@@ -49,4 +51,28 @@ async fn handle_upload(data: warp::multipart::Form) -> Result<impl warp::Reply, 
             ))
         }
     };
+
+    if file.len() == 0 {
+        return Ok(warp::reply::with_status(
+            "File is empty",
+            warp::http::StatusCode::BAD_REQUEST,
+        ));
+    }
+
+    let id = Uuid::new_v4().to_string();
+    let new_file_path = format!("data/{}", id);
+    let mut new_file = match File::create(&new_file_path) {
+        Ok(file) => file,
+        Err(_) => {
+            return Ok(warp::reply::with_status(
+                "Couldn't save file",
+                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            ));
+        }
+    };
+
+    Ok(warp::reply::with_status(
+        &format!("File pasted at {}/data/{}", SITE, id),
+        warp::http::StatusCode::OK,
+    ))
 }
