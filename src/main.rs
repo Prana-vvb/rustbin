50512131de92c430a30d98728e9d73ec78082481
Prana-vvb <pvb1532005@gmail.com>
2024-12-01 15:08:01 +0530

    feat: Upload complete

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use futures::{StreamExt, TryStreamExt};
-use std::convert::Infallible;
+use bytes::BufMut;
 use std::fs::{self, File};
 use std::io::{self, Write};
-use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use uuid::Uuid;
 use warp::{Filter, Rejection, Reply};
@@ -17,28 +16,44 @@ async fn main() {
         .and(warp::multipart::form().max_length(MAX_SIZE))
         .and_then(handle_upload);
 
-    let disp = warp::get()
-        .and(warp::path!("data" / String))
-        .and_then(handle_disp);
+    //let disp = warp::get()
+    //    .and(warp::path!("data" / String))
+    //    .and_then(handle_disp);
 
-    //Why block bots tho
+    //Let the bots have all your data hehehe
     let robots_txt = warp::path("robots.txt").map(|| {
         warp::reply::with_header("User-agent: *\nDisallow: /", "Content-Type", "text/plain")
     });
 
-    let routes = upload.or(disp).or(robots_txt);
+    let routes = upload.or(robots_txt);
 
     let port = 8080;
     warp::serve(routes).run(([127, 0, 0, 1], port)).await;
 }
 
-async fn handle_upload(data: warp::multipart::FormData) -> Result<impl warp::Reply, Infallible> {
+async fn handle_upload(data: warp::multipart::FormData) -> Result<impl Reply, Rejection> {
     let mut parts = data.into_stream();
 
     while let Some(Ok(p)) = parts.next().await {
-        let value = p.stream().try_fold(Vec::new(), |mut vec, data| {
-            vec.put(data);
-            async move { Ok(vec) }
-        }).await
+        let value = p
+            .stream()
+            .try_fold(Vec::new(), |mut vec, data| {
+                vec.put(data);
+                async move { Ok(vec) }
+            })
+            .await
+            .map_err(|e| {
+                eprintln!("{}", e);
+                warp::reject::reject()
+            })?;
+
+        let file_name = format!("./data/{}", Uuid::new_v4());
+        tokio::fs::write(&file_name, value).await.map_err(|e| {
+            eprintln!("{}", e);
+            warp::reject::reject()
+        })?;
+        println!("Created file: {}/data/{}", SITE, file_name);
     }
+
+    Ok("Pasted")
 }
